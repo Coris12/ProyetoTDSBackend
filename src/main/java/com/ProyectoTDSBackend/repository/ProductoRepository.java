b6737f9afa029ce635848487496907020438a8ff
Danicho23 <68290009+DanielBrito23@users.noreply.github.com>
2021-05-05 16:15:06 -0500

    Funcionalidad

---
+++ src/main/java/com/ProyectoTDSBackend/repository/ProductoRepository.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ProyectoTDSBackend.repository;
+
+import com.ProyectoTDSBackend.models.Producto;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author LENOVO
+ */
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    Optional<Producto> findByNombre(String nombre);
+    boolean existsByNombre(String nombre);
+}
---
+++ src/main/java/com/ProyectoTDSBackend/util/CreateRoles.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ProyectoTDSBackend.util;
+
+import com.ProyectoTDSBackend.security.enums.RolNombre;
+import com.ProyectoTDSBackend.security.models.Rol;
+import com.ProyectoTDSBackend.security.service.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author LENOVO
+ */
+@Component
+public class CreateRoles implements CommandLineRunner {
+
+    @Autowired
+    RolService rolService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+        Rol rolUser = new Rol(RolNombre.ROLE_USER);
+        rolService.save(rolAdmin);
+        rolService.save(rolUser);
+        
+    }
+}
