a2c9b4e79a6fbb9d9b0604f2ffbe8646c31cce77
Coris12 <corina.guzman.est@tecazuay.edu.ec>
2021-07-03 07:08:51 -0500

    rol agregado

--- src/main/java/com/ProyectoTDSBackend/security/controller/AuthController.java
+++ src/main/java/com/ProyectoTDSBackend/security/controller/AuthController.java
@@ -79,6 +79,7 @@ public class AuthController {
         usuarioService.save(usuario);
         return new ResponseEntity(new Mensaje("usuario guardado"), HttpStatus.CREATED);
     }
+    
 
     @PostMapping("/login")
     public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
