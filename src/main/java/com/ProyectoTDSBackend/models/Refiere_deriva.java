163306dfd9a8f262f32aced139bd6e210c9bd2be
@Danicho23 <henry.brito.est@tecazuay.edu.ec>
2022-08-28 16:02:47 -0500

    update

--- src/main/java/com/ProyectoTDSBackend/models/Refiere_deriva.java
+++ src/main/java/com/ProyectoTDSBackend/models/Refiere_deriva.java
@@ -5,10 +5,46 @@
  */
 package com.ProyectoTDSBackend.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author LENOVO
  */
+@Entity
+@Table(name = "refiere_deriva")
 public class Refiere_deriva {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_refiere", nullable = false)
+    private int id_refiere;
+
+    @Column(name = "entidad")
+    private String entidad;
+
+    @Column(name = "establecimiento")
+    private String establecimiento;
+
+    @Column(name = "servicio")
+    private String servicio;
+
+    @Column(name = "especialidad")
+    private String especialidad;
+
+    @Column(name = "dia")
+    private String dia;
+    
+    @Column(name = "mes")
+    private String mes;
+    
+    @Column(name = "anio")
+    private String anio;
+    
+ 
 }
